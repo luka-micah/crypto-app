bfa0b649fd52114eed721acfb83a3ecb68ce5fc5
lukamicahzamani <149020825+luka-micah@users.noreply.github.com>
2025-11-06 10:52:32 -0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:crypto_app/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+        textTheme: GoogleFonts.quicksandTextTheme(),
+      ),
+      routes: {"/home": (context) => HomePage()},
+      initialRoute: "/home",
+    );
+  }
+}
